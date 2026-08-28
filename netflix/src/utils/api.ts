2236fc4141e28a92660bebcf9c4c94bd55a74edd
MayHyeyeonKim <devmay202@gmail.com>
2026-08-28 00:58:31 -0700

    added axios baseURL

--- netflix/src/utils/api.ts
+++ netflix/src/utils/api.ts
@@ -4,7 +4,9 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 const api = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${API_KEY}`
-    }
-})
+        Accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+    },
+});
+
+export default api;
